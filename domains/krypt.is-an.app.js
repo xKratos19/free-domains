17e812196ca5f42722c3ea68a35824928b0bd333
xKratos19 <123822288+xKratos19@users.noreply.github.com>
2023-10-23 23:59:13 +0300

    Create krypt.is-an.app.js

---
+++ domains/krypt.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A preview page for my Krypt page(cryptocurrency page)',
+  domain: 'is-an.app',
+  subdomain: 'krypt',
+  owner: {
+    email: 'turcuoctavian30@gmail.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: false
+});
